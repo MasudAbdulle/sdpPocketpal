2ece54275d6ef9c5d1c931c0c85ba5f6ba31fc36
MasudAbdulle <79256237+MasudAbdulle@users.noreply.github.com>
2023-02-10 10:11:29 +0000

    printing api response

--- inflation.php
+++ inflation.php
@@ -4,13 +4,15 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     var country = 'United States'
-$.ajax({
+    $.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/inflation?country=' + country,
     headers: { 'X-Api-Key': 'UAsZ1KG3XMyWZfyWsmTe1A==xVVVPZqVQII6JaXH'},
     contentType: 'application/json',
     success: function(result) {
-        console.log(result);
+        var resultDiv = document.createElement("div");
+        resultDiv.innerHTML = result;
+        document.body.appendChild(resultDiv);
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
@@ -18,7 +20,5 @@ $.ajax({
 });
 
 </script>
-
-<div id = "inflation result">hrnotrngeroingerndo</div>
 </body>
 </html>
