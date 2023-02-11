fbcb51b2da0bf9e76792de0504c48f1c80466b4f
MasudAbdulle <79256237+MasudAbdulle@users.noreply.github.com>
2023-02-11 15:47:29 +0000

    made inflation rate api work

--- inflation.php
+++ inflation.php
@@ -3,17 +3,19 @@
 <body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
-    var country = 'United States'
+    var country = 'Great Britain'
     $.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/inflation?country=' + country,
     headers: { 'X-Api-Key': 'UAsZ1KG3XMyWZfyWsmTe1A==xVVVPZqVQII6JaXH'},
     contentType: 'application/json',
     success: function(result) {
-        var resultDiv = document.createElement("div");
-        resultDiv.innerHTML = result;
-        document.body.appendChild(resultDiv);
-    },
+    console.log(result);
+    var resultDiv = document.createElement("div");
+    resultDiv.innerHTML = result[0].country + ' ' + result[0].period + ' ' + result[0].yearly_rate_pct;
+    document.body.appendChild(resultDiv);
+}
+,
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
     }
