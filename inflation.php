fb2acc0cc7e6332f9c8f059c7476efabe3cfef44
MasudAbdulle <79256237+MasudAbdulle@users.noreply.github.com>
2023-02-14 13:58:05 +0000

    added progress bar

--- inflation.php
+++ inflation.php
@@ -1,7 +1,16 @@
 <html>
-
+<head>
+    <style>
+        #container1{
+            width:400px;
+            height: 400px;
+        }
+    </style>
+</head>
 <body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script>
     var country = 'Great Britain'
     $.ajax({
@@ -21,6 +30,19 @@
     }
 });
 
+
+
+
 </script>
+
+<div class  = "container1" >
+    <h2> normal container </h2>
+    <p>What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+</p>
+
+
+</div>
+
 </body>
 </html>
